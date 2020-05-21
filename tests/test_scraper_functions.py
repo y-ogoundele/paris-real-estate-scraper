c0468ca444af726ebdcfb74d3711b959391e3c42
yogoundele <yogoundele@c02zf1w0lvdn.corpad.adbkng.com>
2020-05-21 16:23:49 +0200

    :sparkles: Create extract blocks function

--- tests/test_scraper_functions.py
+++ tests/test_scraper_functions.py
@@ -1,7 +1,8 @@
 import unittest
 import requests
+import re
 from bs4 import BeautifulSoup
-from scraper_functions import list_property_urls
+from scraper_functions import list_property_urls, extract_blocks, scrape_meta_information
 
 
 class TestListPropertyURLs(unittest.TestCase):
@@ -20,3 +21,39 @@ class TestListPropertyURLs(unittest.TestCase):
                 self.assertEqual(property_urls[i][24:55], 'annonces/locations/appartement/')
 
 
+class TestExtractBlocks(unittest.TestCase):
+    def setUp(self) -> None:
+        self.dummy_url = "https://www.wikipedia.org"
+        self.property_url =  "https://www.seloger.com/annonces/locations/appartement/paris-5eme-75/saint-victor/158755387.htm?projects=1&types=1&places=[{ci:750105}]&rooms=1&enterprise=0&qsVersion=1.0&bd=ListToDetail"
+        self.header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,'referer':'https://www.google.com/'}
+        response = requests.get(self.property_url, headers=self.header)
+        dummy_response = requests.get(self.dummy_url, headers=self.header)
+        self.property_soup = BeautifulSoup(response.text, "html.parser")
+        self.dummy_soup = BeautifulSoup(dummy_response.text, "html.parser")
+
+    def test_empty_soup(self):
+        with self.assertRaises(IndexError):
+            extract_blocks(self.dummy_soup)
+
+    def test_correct_soup(self):
+        self.assertEqual(len(extract_blocks(self.property_soup)), 6)
+
+class TestScrapeMetaInformation(unittest.TestCase):
+    def setUp(self) -> None:
+        self.property_url =  "https://www.seloger.com/annonces/locations/appartement/paris-5eme-75/saint-victor/158755387.htm?projects=1&types=1&places=[{ci:750105}]&rooms=1&enterprise=0&qsVersion=1.0&bd=ListToDetail"
+        self.header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,'referer':'https://www.google.com/'}
+        response = requests.get(self.property_url, headers=self.header)
+        self.property_soup = BeautifulSoup(response.text, "html.parser")
+        main_tag = self.property_soup.find_all('div', class_=re.compile("^app__CWrapMain-aroj7e-3"))[0]
+        self.summary_block = main_tag.find_all('div', class_=re.compile("^Summarystyled__MainWrapper-tzuaot-1"))[0]
+
+    def test_returns_meta_info(self):
+        # TODO: Find a way to get a valid property (studio only)
+        meta_info = scrape_meta_information(self.property_soup, self.summary_block)
+        expected_result = (
+            'Location Studio Paris 5ème - Appartement F1/T1/1 pièce 19,92m² 835€/mois - SeLoger',
+            'https://www.seloger.com/annonces/locations/appartement/paris-5eme-75/saint-victor/158755387.htm',
+            '19,92m²',
+            '1 pièce',
+                           )
+        self.assertEqual(meta_info, expected_result)
